4bd227f745a06489fcf893953d59b867392d4b42
hanodims <hanodims@hotmail.com>
2020-08-16 17:12:56 +0300

    updated

--- calculator.py
+++ calculator.py
@@ -2,29 +2,25 @@
 def main():
 	#write your code here
 	n1 = input("Enter the first number: ")
-	while not n1.isdigit():
-		print(" %s is invalid" % n1)
-		n1 = input("Enter the first number: ")
+	n1v = n1.isdigit()
 	n2 = input("Enter the second number: ")
-	while not n2.isdigit():
-		print(" %s is invalid" % n2)
-		n2 = input("Enter the second number: ")
+	n2v = n2.isdigit()
 	oper = input("Choose the operation (+, -, /, *): ")
-	while oper not in ("+","-","/","*"):
-		print(" %s is invalid" % oper)
-		oper = input("Choose the operation (+, -, /, *): ")
-
-
-	if oper == "*" :
-		print(int(n1) * int(n2))
-	if oper == "+" :
-		print(int(n1) + int(n2))
-	if oper == "-" :
-		print(int(n1) - int(n2))
-	if oper == "/" :
-		if int(n2) == 0:
-			print(" %s is invalid , %s deivded by zero is undefined" % (n2,n1))
-		else: print(int(n1) / int(n2))
+	if oper in ("+","-","/","*"):
+		if n1v == True and n2v == True:
+			if oper == "*" :
+				print(int(n1) * int(n2))
+			elif oper == "+" :
+				print(int(n1) + int(n2))
+			elif oper == "-" :
+				print(int(n1) - int(n2))
+			elif oper == "/" :
+				if int(n2) == 0:
+					print(" %s is invalid , %s deivded by zero is undefined" % (n2,n1))
+				else: print(int(n1) / int(n2))
+		else : print("number is invalid")
+	else: print("operation is not valid")
+		
 			
 
 	
