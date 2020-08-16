00e4d94a38901a9589aaf40ae153c9368acb30cd
hanodims <hanodims@hotmail.com>
2020-08-16 15:22:54 +0300

    calculator

--- calculator.py
+++ calculator.py
@@ -1,7 +1,35 @@
 
 def main():
 	#write your code here
-	pass
+	n1 = input("Enter the first number: ")
+	while not n1.isdigit():
+		print(" %s is invalid" % n1)
+		n1 = input("Enter the first number: ")
+	n2 = input("Enter the second number: ")
+	while not n2.isdigit():
+		print(" %s is invalid" % n2)
+		n2 = input("Enter the second number: ")
+	oper = input("Choose the operation (+, -, /, *): ")
+	while oper not in ("+","-","/","*"):
+		print(" %s is invalid" % oper)
+		oper = input("Choose the operation (+, -, /, *): ")
+
+
+	if oper == "*" :
+		print(int(n1) * int(n2))
+	if oper == "+" :
+		print(int(n1) + int(n2))
+	if oper == "-" :
+		print(int(n1) - int(n2))
+	if oper == "/" :
+		while int(n2) == 0:
+			print(" %s is invalid , %s deivded by zero is undefined" % (n2,n1))
+			n2 = input("Enter the second number: ")
+		else: print(int(n1) / int(n2))
+			
+
+	
+
 	
 
 
